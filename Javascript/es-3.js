2c3fd0bd7063db80aa0d92e6a757d05edcd8b08b
unknown <nicholaspesaresi@gmail.com>
2022-03-18 17:09:53 +0100

    es-3

---
+++ Javascript/es-3.js
@@ -0,0 +1,10 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  students.push(student)
+}
+
+addStudent('Marco');
+console.log(students);
+
+//è possibile aggiungere l'elemento perchè non sto ne ridichiarando ne riassegnando la variabile ma sto aggiungendo l'item alla lista già esistente
