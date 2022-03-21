793370071bf5bb00e7e394aa44e4a1822099e441
unknown <nicholaspesaresi@gmail.com>
2022-03-21 13:26:39 +0100

    es-16

---
+++ Javascript/es-16.js
@@ -0,0 +1,21 @@
+function createStore() {
+    const storeList = [];
+  return function (name){
+        storeList.push(`${name.id}:${name.name}`)
+        console.log(storeList)
+    }
+   
+  }
+  
+  const redPants = { id: 1, name: 'Red Pants' };
+  const whiteHat = { id: 2, name: 'White Hat' };
+  const blackSneakers = { id: 3, name: 'Black Sneakers' };
+  
+  console.log('--- Dress Store ---');
+  const dressStore = createStore();
+  dressStore(redPants);
+  dressStore(whiteHat)
+
+  console.log('--- Shoes Store ---');
+  const shoesStore = createStore();
+  shoesStore(blackSneakers);
