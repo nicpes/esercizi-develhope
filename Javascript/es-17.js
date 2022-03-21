8977d1dadbf86b24e95da22a266a81173ff748e4
unknown <nicholaspesaresi@gmail.com>
2022-03-21 18:13:11 +0100

    es-17

---
+++ Javascript/es-17.js
@@ -0,0 +1,31 @@
+result = 0;
+
+let add = function(num){
+    return result + num
+}
+
+
+
+function calculate() {
+
+
+}
+  
+
+
+  const calculator = calculate();
+  calculator.add(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
