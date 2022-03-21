6037aa41924989917a7faeeffb1d790dc54a5c55
unknown <nicholaspesaresi@gmail.com>
2022-03-21 21:28:24 +0100

    es-17

--- Javascript/es-17.js
+++ Javascript/es-17.js
@@ -1,31 +1,36 @@
-result = 0;
-
-let add = function(num){
-    return result + num
-}
-
-
 
 function calculate() {
+  let result = 0;
+  
+  let calculator = {
+    add : function(num){
+       result += num;
+       return this;
+    },
 
+    sub: function(num){
+      result -= num;
+      return this;
+    },
+
+    multiply : function(num){
+      result *= num;
+      return this;
+    },
+
+    divide : function(num){
+      result /= num;
+      return this;
+    },
+    
+    printResult : function(){
+      return result
+    }
+  }
+  return calculator
 
 }
-  
 
-
-  const calculator = calculate();
-  calculator.add(2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const calculator = calculate();
+result = calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
+console.log(result)
