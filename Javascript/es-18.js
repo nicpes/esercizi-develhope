9b8f4a8a5ea34e52bbe4c27ae6346bc2dbb5712e
unknown <nicholaspesaresi@gmail.com>
2022-03-22 07:35:32 +0100

    es-18

---
+++ Javascript/es-18.js
@@ -0,0 +1,30 @@
+function memoize(fn) {
+    let cache = {};
+      return (...args) => {
+        let i = args[0];
+        if (i in cache) {
+          console.log('Fetching from cache', i);
+          return cache[i];
+        }
+        else {
+          console.log('Calculating result for', i);
+          let result = fn(i);
+          cache[i] = result;
+          return result;
+        }
+      }
+    }
+  
+
+  function factorial(x) {
+    if (x === 0) {
+      return 1;
+    }
+    return x * factorial(x - 1);
+  }
+  
+  
+  factorial = memoize(factorial);
+  console.log(factorial(10));
+  console.log(factorial(6));
+  console.log(factorial(5));
