ec97331ea6ccb8ab4ccebe6ab701dde65b24abc9
unknown <nicholaspesaresi@gmail.com>
2022-03-22 13:16:54 +0100

    es-27

---
+++ Javascript/es-27.js
@@ -0,0 +1,15 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    address : {
+        city: "Italy"
+    },
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
+     }
+
+    
+  };
+  
+  console.log(person.address.city); // Argh! Cannot read property 'city' of undefined
+  console.log(person.fullName()); // Argh! Cannot read property 'fullName()' of undefined
