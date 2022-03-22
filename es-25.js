ebe4a9bfb1d02e8333ef966a19531da0892a9bee
unknown <nicholaspesaresi@gmail.com>
2022-03-22 12:59:46 +0100

    es-25!q q ex

--- es-25.js
+++ es-25.js
@@ -1,6 +1,33 @@
 const person = {
-    // ...
-  }
-  
-  console.log(john.fullName()); // John Doe
-  console.log(simon.fullName()); // Simon Collins
+    firstName : "",
+    lastName : "",
+    
+    get first(){
+        return this.firstName;
+    },
+    set first(name){
+        this.firstName = name;
+    },
+    get last(){
+        return this.lastName;
+    },
+    set last(lastname){
+        this.lastName = lastname;
+    },
+
+    fullName : function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+  };
+
+let john = Object.assign({}, person)
+let simon = Object.assign({}, person)
+
+john.firstName = "John";
+john.lastName = "Doe";
+
+simon.firstName = "Simon"
+simon.lastName = "Collins"
+
+console.log(john.fullName()); // John Doe
+console.log(simon.fullName()); // Simon Collins
