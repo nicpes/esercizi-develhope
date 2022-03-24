0e26510b4108bac89bf59eeb27860c64116d62dd
unknown <nicholaspesaresi@gmail.com>
2022-03-24 14:12:36 +0100

    es-36

---
+++ Javascript/es-36.js
@@ -0,0 +1,23 @@
+class Person {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      
+    }
+
+    static fromObject(obj){
+    this.firstName = obj.firstName;
+    this.lastName = obj.lastName;
+    return new Person(this.firstName, this.lastName)
+    }
+  
+}
+  
+  const obj = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+  };
+
+
+  const person = Person.fromObject(obj);
+  console.log(`${person.firstName} ${person.lastName}`);
