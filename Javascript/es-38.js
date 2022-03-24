7cfffc63425daf43cc146373e0eb47cc8358810b
unknown <nicholaspesaresi@gmail.com>
2022-03-24 14:57:04 +0100

    es-38

---
+++ Javascript/es-38.js
@@ -0,0 +1,38 @@
+class BankAccount {
+    amount = 0;
+  
+    constructor(initialAmount) {
+      this.amount = initialAmount;
+    }
+  
+    deposit(amount) {
+      this.amount += amount;
+    }
+    withdraw(amount) {
+      this.amount -= amount;
+    }
+    view() {
+      console.log(this.amount);
+    }
+  }
+ 
+  class BankAccountVip extends BankAccount{
+      constructor(amount){
+          super(amount)
+      }
+     view(){
+         if(this.amount >= 1000){
+       this.amount += ((this.amount * 3) / 100)
+       console.log(this.amount)
+     }
+     else console.log(this.amount)
+     }
+    }
+  
+  const bankAccountVip = new BankAccountVip(1000);
+  bankAccountVip.deposit(500);
+  bankAccountVip.deposit(1200);
+  bankAccountVip.withdraw(800);
+  bankAccountVip.deposit(3500);
+  bankAccountVip.view();
+ 
