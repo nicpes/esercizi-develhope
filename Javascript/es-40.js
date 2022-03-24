1712b9602273a079b807f628c4cdabef9884a56d
unknown <nicholaspesaresi@gmail.com>
2022-03-24 15:18:39 +0100

    es-40

---
+++ Javascript/es-40.js
@@ -0,0 +1,33 @@
+class Person {
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;}
+
+        get firstname() {
+            return this.firstName;
+          }
+
+          set firstname(name) {
+            this.firstName = name;
+          }
+        
+          get lastname() {
+            return this.lastName;
+          }
+        
+         set lastname(name) {
+            this.lastName = name;
+          }
+          
+       get fullName (){
+            return `${this.firstName} ${this.lastName}`
+        }
+    }
+  
+  const person = new Person('Mario', 'Rossi', 25);
+  console.log(person.fullName);
+  
+  person.firstName = 'Maria';
+  person.lastName = 'Verdi';
+  console.log(person.fullName);
