1a1cce74f755c5806113fca896b553c7e86e5f08
unknown <nicholaspesaresi@gmail.com>
2022-03-26 14:13:57 +0100

    es-46

---
+++ Javascript/es-46.js
@@ -0,0 +1,11 @@
+ let isAdult = (person) => console.log(person.age >= 18)
+  
+  const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+  
+isAdult(person)
+
